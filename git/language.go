533e0551d5424589881dea578d65c9f2f2183e13
AndreaGhizzoni <andrea.ghz@gmail.com>
2018-05-27 11:38:00 +0200

    added command to execute to get repositories

--- git/language.go
+++ git/language.go
@@ -3,4 +3,5 @@ package git
 type Language struct {
 	Name         string            `json:"name"`
 	Repositories map[string]string `json:"repositories"`
+	Command      string            `json:"command"`
 }
