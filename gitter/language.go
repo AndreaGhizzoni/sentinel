5ba3c2954143d5eee6ce47dabe3e057fb70f5b1d
AndreaGhizzoni <andrea.ghz@gmail.com>
2018-06-01 16:56:23 +0200

    adapted language structure for projects_folder property

--- gitter/language.go
+++ gitter/language.go
@@ -1,9 +1,10 @@
 package gitter
 
 type Language struct {
-	Name         string            `json:"name"`
-	Repositories map[string]string `json:"repositories"`
-	Command      string            `json:"command"`
+	Name           string            `json:"name"`
+	Repositories   map[string]string `json:"repositories"`
+	Command        string            `json:"command"`
+	ProjectsFolder string            `json:"projects_folder"`
 }
 
 func (l *Language) BuildFolderStructure(base string) error {
@@ -22,6 +23,12 @@ func (l *Language) buildJavaFolderStructure(base string) error {
 	if _, err := createFolderIfNotExists(languagePath); err != nil {
 		return err
 	}
+
+	if len(l.ProjectsFolder) == 0 {
+		l.ProjectsFolder = languagePath
+	} else {
+		l.ProjectsFolder = languagePath + "/" + l.ProjectsFolder
+	}
 	return nil
 }
 
@@ -37,6 +44,12 @@ func (l *Language) buildGoFolderStructure(base string) error {
 		if _, err := createFolderIfNotExists(dir); err != nil {
 			return err
 		}
+	}
+
+	if len(l.ProjectsFolder) == 0 {
+		l.ProjectsFolder = dirs[0]
+	} else {
+		l.ProjectsFolder = dirs[0] + "/" + l.ProjectsFolder
 	}
 	return nil
 }
